0f25f8e6e6ec7f71b73dbce82daa35a3b7ed2b49
Dima Kogan <dima@secretsauce.net>
2015-07-02 13:00:04 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name         = 'gnuplotlib',
+      version      = '0.1',
+      author       = 'Dima Kogan',
+      author_email = 'dima@secretsauce.net',
+      url          = 'http://github.com/dkogan/gnuplotlib',
+      description  = 'Gnuplot-based plotting for numpy',
+      license      = 'LGPL (any version)',
+      py_modules   = ['gnuplotlib'] )
