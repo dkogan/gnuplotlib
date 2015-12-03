b5589a6cd41a1bedc70693cf38366a525a854cae
Dima Kogan <dima@secretsauce.net>
2015-12-02 20:10:16 -0800

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class MoreClean(clean):
 
 
 setup(name         = 'gnuplotlib',
-      version      = '0.9',
+      version      = '0.10',
       author       = 'Dima Kogan',
       author_email = 'dima@secretsauce.net',
       url          = 'http://github.com/dkogan/gnuplotlib',
