f05c8b62022c84e406aca5e59dd595964c88253b
Dima Kogan <dima@secretsauce.net>
2016-05-16 23:54:51 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name         = 'gnuplotlib',
-      version      = '0.11',
+      version      = '0.12',
       author       = 'Dima Kogan',
       author_email = 'dima@secretsauce.net',
       url          = 'http://github.com/dkogan/gnuplotlib',
