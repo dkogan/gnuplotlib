ed18068bcdbec8468f77fe837a1e07edea980e16
Dima Kogan <dima@secretsauce.net>
2016-05-16 23:54:17 -0700

    python setup.py no longer touches README generation

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 
-from distutils.core          import setup
-from distutils.command.clean import clean
-import subprocess
-
-subprocess.call( ['make', 'README', 'README.org'] )
-
-
-class MoreClean(clean):
-    def run(self):
-        clean.run(self)
-        subprocess.call( ['make', 'clean'] )
-
+from distutils.core import setup
 
 setup(name         = 'gnuplotlib',
       version      = '0.11',
@@ -21,4 +10,4 @@ setup(name         = 'gnuplotlib',
       description  = 'Gnuplot-based plotting for numpy',
       license      = 'LGPL-3+',
       py_modules   = ['gnuplotlib'],
-      cmdclass     = {'clean': MoreClean})
+      cmdclass     = {})
