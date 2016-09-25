c16753db1281eedf3ba87ddc93fabe1fd2547f0a
Dima Kogan <dima@secretsauce.net>
2016-09-24 22:19:50 -0700

    added long description in the setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(name         = 'gnuplotlib',
       author_email = 'dima@secretsauce.net',
       url          = 'http://github.com/dkogan/gnuplotlib',
       description  = 'Gnuplot-based plotting for numpy',
+
+      long_description = """gnuplotlib allows numpy data to be plotted using Gnuplot as a backend. As
+much as was possible, this module acts as a passive pass-through to Gnuplot,
+thus making available the full power and flexibility of the Gnuplot backend.""",
+
       license      = 'LGPL-3+',
       py_modules   = ['gnuplotlib'],
       install_requires = ('numpy', 'numpysane >= 0.3'))
