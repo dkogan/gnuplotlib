82440ab1df7d3ad225147bb765905fba9e42b1ab
Dima Kogan <dima@secretsauce.net>
2017-04-28 20:10:20 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name         = 'gnuplotlib',
-      version      = '0.14',
+      version      = '0.15',
       author       = 'Dima Kogan',
       author_email = 'dima@secretsauce.net',
       url          = 'http://github.com/dkogan/gnuplotlib',
