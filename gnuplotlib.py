49f068507edb8d1c36c78a9cbe55658b69e33600
Dima Kogan <dima@secretsauce.net>
2018-09-29 11:14:00 -0700

    version bump

--- gnuplotlib.py
+++ gnuplotlib.py
@@ -739,7 +739,7 @@ import numpy as np
 import numpysane as nps
 
 # setup.py assumes the version is a simple string in '' quotes
-__version__ = '0.22'
+__version__ = '0.23'
 
 # note that 'with' is both a known plot and curve option
 knownPlotOptions = frozenset(('dump', 'ascii', 'log', 'notest', 'wait',
